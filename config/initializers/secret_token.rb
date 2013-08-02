285c395802af41d9f46bf83938cf9e2ccd027f56
root <root@bt.foo.org>
2013-08-02 10:55:40 +0700

    musik edot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdotMusic::Application.config.secret_token = 'bb2fcf8928ec1fdb51dce7036030e548eba3c7e52257d9eb2a6e040a3742455003d09f4f2af018759e56a7fcc983fc0e62e93da177bc40f339b7a9d5e0ad6ef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EdotMusic::Application.config.session_store :cookie_store, key: '_edot_music_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EdotMusic::Application.config.session_store :active_record_store
